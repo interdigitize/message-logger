98c420768f6f4e0828cf4f5b7cf727014a77bb7f
interdigitize <interdigitize@users.noreply.github.com>
2017-11-06 12:03:56 -0800

    Implements async logger

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,5 +1,52 @@
 'use strict'
 
+const P = require("bluebird")
+
+const {
+	sortCandidates,
+	addEntryToPrint,
+	printFromQueue,
+	confirmLogsAreEmpty
+} = require('./util-fns');
+
 module.exports = (logSources, printer) => {
-	// I didn't get to this in the alotted time — which is because promises are beautiful :(
+  console.log('----------------ASYNC-----------------')
+	var printQueue = [];
+	var printCandidatesPromises = [];
+	var printCandidates = []
+	var moreToPrint = true;
+
+		function logAsync(){
+			logSources.forEach((logEntry, index) => {
+				printCandidatesPromises.push(logEntry.popAsync());
+			})
+			Promise.all(printCandidatesPromises)
+			.then((candidates) => {
+				candidates.forEach(logEntry => {
+					if(logEntry) {
+						printCandidates.push(logEntry)
+						printCandidatesPromises.shift();
+					}
+				})
+				sortCandidates(printCandidates);
+				return printCandidates;
+			})
+			.then( printCandidates => {
+				if (printCandidates.length === 0) {
+					moreToPrint = false;
+					printer.done()
+					return confirmLogsAreEmpty(logSources);
+				}
+					addEntryToPrint(printQueue, printCandidates);//this is a bottleneck to refactor
+					printFromQueue(printQueue, printer);
+			})
+			.then(() => {
+				if(moreToPrint){
+					logAsync()
+				}
+			})
+			.catch( err => console.log(err));
+		}
+		
+		logAsync();
 }
